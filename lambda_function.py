3ecdcf5a11a368e2f53aa394147030ad2507b6aa
Subash Chandra <meeetsubash@gmail.com>
2022-11-30 23:58:00 -0600

    added CRUD to lambda
    Added create/read/delete to lambda, along with a test json

--- lambda_function.py
+++ lambda_function.py
@@ -1,23 +1,64 @@
 import boto3
 import json
 
-def lambda_handler(event,context):
+class DBupload:
     
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('labels')
-    
-    iid=event["id"]
-    imageid = event["imageid"]
-    labelnames = event["labelnames"]
-    labeltypes=event["labeltypes"]
-    
-    response = table.put_item(
-        Item={
-            "id":iid,
-            "imageid":imageid,
-            "labeltypes":labeltypes,
-            "labelnames":labelnames
+    def __init__(self):
+        client = boto3.resource('dynamodb')
+        self.table=client.Table('labels')
+        
+    def Create_data(self,event):
+        response=self.table.put_item(
+            Item={
+                'instanceID' : event['id'],
+                'imageID' : event['imageid'],
+                'labels' : event['labels']
+            }
+        )
+        return{
+            'statusCode' : response['ResponseMetadata']['HTTPStatusCode'],
+            'body' : 'Record ' + event['id'] + ' added'
         }
-    )
-    
-    return response
+         
+    def  Read_data(self , event):
+        response = self.table.get_item(
+            Key={
+                'instanceID': event['id']
+            }
+        )
+        if 'Item' in response:
+            return response['Item']
+        else:
+            return {
+                'statusCode': '404',
+                'body': 'Not found'
+            }     
+            
+    def  Delete_data(self , event):
+        response = self.table.delete_item(
+            Key={
+                'instanceID': event['id']
+            }
+        )
+        return {
+                'statusCode': '200',
+                'body': 'Deleted the item with id :' + event['id']
+            }
+
+def lambda_handler(event, context):
+    if event:
+        instance =  DBupload()
+        if event['tasktype']  == "create":
+            create_result =  instance.Create_data(event['data'])
+            return create_result
+        elif event['tasktype']  == "read":
+            read_result =  instance.Read_data(event['data'])
+            return read_result
+        elif event['tasktype']  == "delete":
+            delete_result =  instance.Delete_data(event['data'])
+            return delete_result
+        else :
+            return {
+                'statusCode': '404',
+                'body': 'Not found'
+            }
