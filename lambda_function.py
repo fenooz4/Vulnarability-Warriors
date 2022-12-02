73cfefe8d0a475777c08e7434debf49e4b49c644
Subash Chandra <meeetsubash@gmail.com>
2022-12-02 00:26:38 -0600

    fixed http body error

--- lambda_function.py
+++ lambda_function.py
@@ -48,15 +48,19 @@ class DBupload:
 def lambda_handler(event, context):
     if event:
         instance =  DBupload()
-        if event['tasktype']  == "create":
-            create_result =  instance.Create_data(event['data'])
+        body = json.loads(event['body'])
+        tasktype = body['tasktype']
+        
+        if tasktype  == "create":
+            create_result =  instance.Create_data(body['data'])
             return create_result
-        elif event['tasktype']  == "read":
-            read_result =  instance.Read_data(event['data'])
+        elif tasktype == "read":
+            read_result =  instance.Read_data(body['data'])
             return read_result
-        elif event['tasktype']  == "delete":
-            delete_result =  instance.Delete_data(event['data'])
-            return delete_result
+        elif tasktype == "delete":
+            delete_result =  instance.Delete_data(body['data'])
+            return delete_result   
+            
         else :
             return {
                 'statusCode': '404',
