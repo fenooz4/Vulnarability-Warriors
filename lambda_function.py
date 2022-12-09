a4b386257a4a235a507cf962aec645b1d1f80c50
Subash Chandra <meeetsubash@gmail.com>
2022-12-09 14:17:03 -0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -11,40 +11,21 @@ class DBupload:
         response=self.table.put_item(
             Item={
                 'instanceID' : event['id'],
-                'imageID' : event['imageid'],
-                'labels' : event['labels']
+                'imageID' : event['imgID'],
+                'name' : event['Name'],
+                'ENV' : event['Env'],
+                'Cmd' : event['Cmd'],
+                'Volumes': event['Volumes'],
+                'WorkingDir' : event['WorkingDir'],
+                'EntryPoint' : event['EntryPoint'],
+                'labels': event['labels'],
+                'labelErrors': event['labelErrors']
             }
         )
         return{
             'statusCode' : response['ResponseMetadata']['HTTPStatusCode'],
             'body' : 'Record ' + event['id'] + ' added'
         }
-         
-    def  Read_data(self , event):
-        response = self.table.get_item(
-            Key={
-                'instanceID': event['id']
-            }
-        )
-        if 'Item' in response:
-            return response['Item']
-        else:
-            return {
-                'statusCode': '404',
-                'body': 'Not found'
-            }     
-            
-    def  Delete_data(self , event):
-        response = self.table.delete_item(
-            Key={
-                'instanceID': event['id']
-            }
-        )
-        return {
-                'statusCode': '200',
-                'body': 'Deleted the item with id :' + event['id']
-            }
-
 def lambda_handler(event, context):
     if event:
         instance =  DBupload()
@@ -54,13 +35,6 @@ def lambda_handler(event, context):
         if tasktype  == "create":
             create_result =  instance.Create_data(body['data'])
             return create_result
-        elif tasktype == "read":
-            read_result =  instance.Read_data(body['data'])
-            return read_result
-        elif tasktype == "delete":
-            delete_result =  instance.Delete_data(body['data'])
-            return delete_result   
-            
         else :
             return {
                 'statusCode': '404',
